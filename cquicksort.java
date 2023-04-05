990b154d72227a8f3a4919f3f2eed06cfa50a468
Anshul Arya <92744958+anshularya2303@users.noreply.github.com>
2023-04-06 05:18:28 +0530

    Add files via upload

---
+++ cquicksort.java
@@ -0,0 +1,38 @@
+public class cquicksort {
+    public static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                // swap
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        // swap for pivot
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    public static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pidx = partition(arr, low, high);
+            quickSort(arr, low, pidx - 1);
+            quickSort(arr, pidx + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 7, 9, 8 };
+        int n = arr.length;
+        quickSort(arr, 0, n - 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
